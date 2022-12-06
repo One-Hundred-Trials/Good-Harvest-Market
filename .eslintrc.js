1be465bd70f540c85084134fd8f84d79f93ac6b3
khv2644511 <khv2644511@naver.com>
2022-12-06 20:57:47 +0900

    eslint, prettier 셋팅

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "airbnb",
+    "prettier",
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/destructuring-assignment": [0, "always"],
+    "react/prop-types": "off",
+    "react/jsx-no-useless-fragment": [2, { allowExpressions: true }],
+    "import/no-cycle": "off",
+    "no-console": "off",
+    "no-unused-vars": "off",
+    "no-useless-concat": "off",
+    "react/no-unknown-property": "off",
+    "react/jsx-props-no-spreading": "off",
+  },
+};
