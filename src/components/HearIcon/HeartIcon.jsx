a38c80b6c340340b9933649121271173a9f1f1eb
khv2644511 <khv2644511@naver.com>
2022-12-12 03:35:03 +0900

    Style: Post_Card_component UI 제작

---
+++ src/components/HearIcon/HeartIcon.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+import Heart from '../../assets/img/icon-heart.png';
+
+const HeartStyle = styled.span`
+  display: flex;
+  align-items: center;
+  gap: 7.82px;
+  color: var(--main-grey-76);
+`;
+
+const HeartImgStyle = styled.img`
+  width: 16.36px;
+  height: 14.55px;
+`;
+
+export default function HeartIcon() {
+  return (
+    <HeartStyle>
+      <HeartImgStyle src={Heart} alt="" />
+      <span>58</span>
+    </HeartStyle>
+  );
+}
