0718b199c352ef091cc07dca69d42ab0f50815d0
konveloper <konveloper@gmail.com>
2022-12-13 02:08:30 +0900

    Modify : 컴포넌트 스타일 파일 분리

--- src/components/Comment/CommentInput/CommentInput.jsx
+++ src/components/Comment/CommentInput/CommentInput.jsx
@@ -1,46 +1,16 @@
 import React from 'react';
-import styled from 'styled-components';
 import ProfileImg from '../../../assets/img/basic-profile-36.png';
-
-const CommentContainer = styled.div`
-  width: 390px;
-  height: 61px;
-`;
-const CommentForm = styled.form`
-  display: flex;
-  justify-content: space-between;
-`;
-const MyProfileImg = styled.img`
-  width: 36px;
-  height: 36px;
-  margin: 13px 18px 12px 16px;
-`;
-const CommentTxtInput = styled.input`
-  width: 100%;
-  font-size: 1.4rem;
-  font-weight: 400;
-  border-style: none;
-  outline: none;
-  &::placeholder {
-    font-size: 1.4rem;
-    font-weight: 400;
-    color: var(--sub-grey-C4);
-  }
-`;
-const UploadBtn = styled.button`
-  flex-shrink: 0;
-  padding: 16px;
-  background: inherit;
-  border: none;
-  font-size: 1.4rem;
-  font-weight: 500;
-  color: var(--sub-grey-C4);
-  /* cursor: pointer; */
-`;
+import {
+  CommentContainerDiv,
+  CommentForm,
+  MyProfileImg,
+  CommentTxtInput,
+  UploadBtn,
+} from './CommentInputStyle';
 
 export default function Comment() {
   return (
-    <CommentContainer>
+    <CommentContainerDiv>
       <CommentForm>
         <MyProfileImg src={ProfileImg} alt="프로필 사진" />
         <CommentTxtInput
@@ -52,6 +22,6 @@ export default function Comment() {
           게시
         </UploadBtn>
       </CommentForm>
-    </CommentContainer>
+    </CommentContainerDiv>
   );
 }
