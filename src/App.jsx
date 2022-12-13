472856322049546988d60db0c6b5010967948a0e
khv2644511 <khv2644511@naver.com>
2022-12-13 10:31:14 +0900

    Feat : Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,32 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { GlobalStyle } from './styles/GlobalStyles';
-import Home from './pages/Main/Home/Home'
+import Home from './pages/Main/Home/Home';
 import Login from './pages/Login/Login';
-import Signup from "./pages/Signup/Signup";
-import Search from './pages/Main/Home/Search/Search'
-import UserProfile from './pages/Main/Profile/UserProfile/UserProfile'
-import MyProfile from './pages/Main/Profile/MyProfile/MyProfile'
-import ProfileEdit from './pages/Main/Profile/MyProfile/ProfileEdit/ProfileEdit'
-import ProductUpload from './pages/Main/Profile/MyProfile/Product/ProductUpload/ProductUpload'
-import Post from './pages/Main/Post/Post'
-import PostUpload from './pages/Main/Post/PostUpload/PostUpload'
-import Chat from './pages/Main/Chat/Chat'
-import ChatRoom from './pages/Main/Chat/ChatRoom/ChatRoom'
-import NotFound from './pages/NotFound/NotFound'
+import MyProfile from './pages/Main/Profile/MyProfile/MyProfile';
+import Post from './pages/Main/Post/Post';
+import NotFound from './pages/NotFound/NotFound';
+import Root from './components/Root';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: '/login', element: <Login /> },
+      { path: '/post/:id', element: <Post /> },
+      { path: '/my_profile', element: <MyProfile /> },
+    ],
+  },
+]);
 
 function App() {
   return (
     <React.StrictMode>
       <GlobalStyle />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/login" element={<Login />}/>
-          <Route path="/login/login_email" element={<Login />}/>
-          <Route path="/login/sign_up" element={<Signup />}/>
-          <Route path="/search" element={<Search />}/>
-          <Route path="/user_profile/:id" element={<UserProfile />}/>
-          <Route path="/my_profile" element={<MyProfile />}/>
-          <Route path="/profile_edit" element={<ProfileEdit />}/>
-          <Route path="/product_upload" element={<ProductUpload />}/>
-          <Route path="/post/:id" element={<Post />}/>
-          <Route path="/post_upload" element={<PostUpload />}/>
-          <Route path="/chat" element={<Chat />}/>
-          <Route path="/chat/:id" element={<ChatRoom />}/>
-          <Route path="/not_found" element={<NotFound />}/>
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </React.StrictMode>
   );
 }
--- src/pages/Main/Home/Home.jsx
+++ src/pages/Main/Home/Home.jsx
@@ -1,13 +1,6 @@
 import React from 'react';
 import PostCard from '../../../components/PostCard/PostCard';
-import MyProfile from '../Profile/MyProfile/MyProfile';
 
 export default function Home() {
-  return (
-    <div>
-      <MyProfile />
-      <PostCard />
-      Home
-    </div>
-  );
+  return <div>Home</div>;
 }
