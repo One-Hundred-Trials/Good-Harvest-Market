ccf3694c34034b4652108a3b3856ee847ecc8b5b
khv2644511 <khv2644511@naver.com>
2022-12-13 22:24:31 +0900

    Modify : LoginEmailStyle.jsx의 버튼 마진 변경

--- src/pages/Login/LoginEmail/LoginEmail.jsx
+++ src/pages/Login/LoginEmail/LoginEmail.jsx
@@ -1,47 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
 import Button from '../../../components/Button/Button';
-import { Wrap } from '../../../styles/GlobalStyles';
-
-const ContSecStyle = styled.section`
-  ${Wrap}
-  text-align: center;
-  /* background-color: var(--sub-grey-C4); */
-  color: var(--main-grey-76);
-`;
-
-const HeaderStyle = styled.h1`
-  font-weight: 500;
-  font-size: 2.4rem;
-  line-height: 30px;
-  padding: 30px 0 40px 0;
-  color: black;
-`;
-
-const InputFormStyle = styled.form`
-  display: flex;
-  flex-direction: column;
-  width: 322px;
-  margin: 0 auto;
-  text-align: left;
-  label {
-  }
-  input {
-    width: 322px;
-    height: 33px;
-    border: transparent;
-    border-bottom: 1px solid #dbdbdb;
-    margin-bottom: 16px;
-    outline: none;
-    &:focus {
-      border-bottom: 2px solid var(--point-green);
-    }
-  }
-`;
-const BtnComStyle = styled.Button`
-  margin: 20px;
-  background-color: red;
-`;
+import {
+  ContSecStyle,
+  HeaderStyle,
+  InputFormStyle,
+  BtnComStyle,
+} from './LoginEmailStyle';
 
 export default function LoginEmail() {
   return (
