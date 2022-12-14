3ee7c44a698111125e3e5747093b36b6538299fc
mixnuts211 <migyeong0211@gmail.com>
2022-12-14 20:03:47 +0900

    S tyle : Product upload, edit page 제작

---
+++ src/pages/Main/Profile/MyProfile/Product/ProductEdit/ProductEditStyle.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import { Wrap } from '../../../../../../styles/GlobalStyles';
+
+export const ProductUploadContStyle = styled.div`
+  ${Wrap}
+`;
+export const ProductUploadSectionStyle = styled.section`
+  padding: 30px 34px;
+`;
