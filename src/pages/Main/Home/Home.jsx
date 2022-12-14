11cf6ee725ab1c542dd5e670048762679f520b4b
mixnuts211 <migyeong0211@gmail.com>
2022-12-15 00:49:47 +0900

    Style : Home page UI 제작

--- src/pages/Main/Home/Home.jsx
+++ src/pages/Main/Home/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import PostCard from '../../../components/PostCard/PostCard';
-import MyProfile from '../Profile/MyProfile/MyProfile';
+import TopMainNav from '../../../components/Header/TopMainNav/TopMainNav';
+import HomeRenderBlank from '../../../components/HomeRender/HomeRenderBlank';
+import HomeRenderHomeFeed from '../../../components/HomeRender/HomeRenderHomeFeed';
+import Nav from '../../../components/Nav/Nav';
 
 export default function Home() {
   return (
     <div>
-      <MyProfile />
-      <PostCard />
-      Home
+      <TopMainNav />
+      {/* 조건부 렌더링 HomeRenderBlank 또는 HomeRenderFeed */}
+      <Nav />
     </div>
   );
 }
