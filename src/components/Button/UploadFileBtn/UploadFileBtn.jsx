818068cbdcdd548680a9a3b7aa279b5b9f9e3769
konveloper <konveloper@gmail.com>
2022-12-15 02:18:36 +0900

    Modify : UploadFileBtn 컴포넌트 margin props 추가

--- src/components/Button/UploadFileBtn/UploadFileBtn.jsx
+++ src/components/Button/UploadFileBtn/UploadFileBtn.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { UploadFileLabel } from './UploadFileBtnStyle.jsx';
 
-export default function UploadFileBtn() {
+export default function UploadFileBtn(props) {
   return (
-    <UploadFileLabel htmlFor="file">
+    <UploadFileLabel htmlFor="file" style={{ margin: props.margin }}>
       <input type="file" id="file" accept="image/*" />
     </UploadFileLabel>
   );
