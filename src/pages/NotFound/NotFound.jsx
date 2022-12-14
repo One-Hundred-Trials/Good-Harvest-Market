d7fd3a481612bf051a8cd6725476991b1713d63a
mixnuts211 <migyeong0211@gmail.com>
2022-12-15 00:30:46 +0900

    Modify : NotFound 페이지 콘텐츠 Blank component로 교체

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import icon404Img from '../../assets/img/icon-404.png';
-import Button from '../../components/Button/Button';
-import {
-  ContainerSectionStyle,
-  NotFoundImgStyle,
-  NotFoundTextStyle,
-} from './NotFoundStyle';
+import Blank from '../../components/Blank/Blank';
+import ContainerSectionStyle from './NotFoundStyle';
 
 export default function NotFound() {
   return (
     <ContainerSectionStyle>
-      <NotFoundImgStyle src={icon404Img} />
-      <NotFoundTextStyle>요청하신 페이지를 찾을 수 없어요!</NotFoundTextStyle>
-      <Button size="m" variant="abled">
+      <Blank src={icon404Img} text="요청하신 페이지를 찾을 수 없어요!">
         이전 페이지
-      </Button>
+      </Blank>
     </ContainerSectionStyle>
   );
 }
--- src/pages/NotFound/NotFoundStyle.jsx
+++ src/pages/NotFound/NotFoundStyle.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import styled from 'styled-components';
 
-export const ContainerSectionStyle = styled.section`
+const ContainerSectionStyle = styled.section`
   position: absolute;
   top: 50%;
   left: 50%;
@@ -9,13 +8,5 @@ export const ContainerSectionStyle = styled.section`
   padding: 0 72px;
   text-align: center;
 `;
-export const NotFoundImgStyle = styled.img`
-  width: 310px;
-  vertical-align: top;
-  margin-bottom: 15px;
-`;
-export const NotFoundTextStyle = styled.p`
-  margin-bottom: 50px;
-  font-size: 1.5rem;
-  color: var(--main-grey-76);
-`;
+
+export default 'ContainerSectionStyle';
