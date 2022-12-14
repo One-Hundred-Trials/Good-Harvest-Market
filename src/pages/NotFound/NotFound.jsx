ee75d4221bc2aaf0caf2480d3aaae38146b515a8
mixnuts211 <migyeong0211@gmail.com>
2022-12-15 00:54:29 +0900

    Modify : NotFound 파일 구조 및 이미지 사이즈 수정

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
 import icon404Img from '../../assets/img/icon-404.png';
 import Blank from '../../components/Blank/Blank';
-import ContainerSectionStyle from './NotFoundStyle';
+
+const ContainerSectionStyle = styled.section`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 0 72px;
+  text-align: center;
+`;
 
 export default function NotFound() {
   return (
     <ContainerSectionStyle>
-      <Blank src={icon404Img} text="요청하신 페이지를 찾을 수 없어요!">
+      <Blank
+        src={icon404Img}
+        width="300px"
+        text="요청하신 페이지를 찾을 수 없어요!"
+      >
         이전 페이지
       </Blank>
     </ContainerSectionStyle>
