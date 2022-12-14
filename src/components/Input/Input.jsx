5b4bd226d86ad494f83ed2537d43deca93bcb824
HYEBIN KIM <khv2644511@naver.com>
2022-12-14 14:12:35 +0900

    Modify : Input.jsx 코드 수정

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -3,9 +3,9 @@ import { ContDivStyle, InputStyle } from './InputStyle';
 
 export default function Input({ label, type }) {
   return (
-    <div>
+    <ContDivStyle>
       <label htmlFor={type}>{label}</label>
       <InputStyle type={type || 'text'} id={type} />
-    </div>
+    </ContDivStyle>
   );
 }
