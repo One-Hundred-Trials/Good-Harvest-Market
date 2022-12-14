aa7a3dab664a8f4e5f0e105e1f3d2194d204a20a
konveloper <konveloper@gmail.com>
2022-12-15 02:19:21 +0900

    Modify : ChatComment 스타일 컴포넌트 파일 분리

--- src/components/Chat/ChatRoom/ChatComment/ChatComment.jsx
+++ src/components/Chat/ChatRoom/ChatComment/ChatComment.jsx
@@ -1,46 +1,15 @@
 import React from 'react';
-import styled from 'styled-components';
 import UploadFileBtn from '../../../Button/UploadFileBtn/UploadFileBtn';
+import {
+  MessageFormStyle,
+  MessageInputStyle,
+  SendBtn,
+} from './ChatCommentStyle';
 
-const MessageFormStyle = styled.form`
-  display: flex;
-  justify-content: space-between;
-  height: 61px;
-  width: 100%;
-  border-top: 1px solid var(--sub-grey-C4);
-  bottom: 0;
-  position: fixed;
-  background: #fff;
-`;
-
-const MessageInputStyle = styled.input`
-  width: 100%;
-  font-size: 1.4rem;
-  font-weight: 400;
-  border-style: none;
-  outline: none;
-  padding: 0;
-  &::placeholder {
-    font-size: 1.4rem;
-    font-weight: 400;
-    color: var(--sub-grey-C4);
-  }
-`;
-
-const SendBtn = styled.button`
-  flex-shrink: 0;
-  padding: 16px;
-  border: none;
-  font-size: 1.4rem;
-  font-weight: 500;
-  color: var(--sub-grey-C4);
-  background: inherit;
-`;
-
-export default function ChatComment() {
+export default function ChatComment(props) {
   return (
     <MessageFormStyle>
-      <UploadFileBtn />
+      <UploadFileBtn margin="12px 18px 13px 16px" />
       <MessageInputStyle
         type="text"
         id="commentInput"
---
+++ src/components/Chat/ChatRoom/ChatComment/ChatCommentStyle.jsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const MessageFormStyle = styled.form`
+  display: flex;
+  justify-content: space-between;
+  height: 61px;
+  width: 100%;
+  border-top: 1px solid var(--sub-grey-C4);
+  bottom: 0;
+  position: fixed;
+  background: #fff;
+`;
+
+export const MessageInputStyle = styled.input`
+  width: 100vh;
+  font-size: 1.4rem;
+  font-weight: 400;
+  border-style: none;
+  outline: none;
+  padding: 0;
+  &::placeholder {
+    font-size: 1.4rem;
+    font-weight: 400;
+    color: var(--sub-grey-C4);
+  }
+`;
+
+export const SendBtn = styled.button`
+  flex-shrink: 0;
+  padding: 16px;
+  border: none;
+  font-size: 1.4rem;
+  font-weight: 500;
+  color: var(--sub-grey-C4);
+  background: inherit;
+`;
