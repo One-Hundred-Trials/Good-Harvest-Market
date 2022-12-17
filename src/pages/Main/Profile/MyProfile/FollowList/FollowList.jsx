96dedc9d30a01d75a1f430d8942c7b73237327db
mixnuts211 <migyeong0211@gmail.com>
2022-12-17 20:48:36 +0900

    Modify : Profile tab 페이지  컨테이너 레이아웃 및 컴포넌트 재설정

--- src/pages/Main/Profile/MyProfile/FollowList/FollowList.jsx
+++ src/pages/Main/Profile/MyProfile/FollowList/FollowList.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
+import { ConWrap } from '../../../../../styles/GlobalStyles';
 import FollowUserList from '../../../../../components/FollowUserList/FollowUserList';
 import TopChatNav from '../../../../../components/Header/TopChatNav/TopChatNav';
-import Nav from '../../../../../components/Nav/Nav';
 
 export default function FollowList() {
   return (
-    <React.Fragment>
+    <>
       <TopChatNav>내 이웃들</TopChatNav>
       <FollowUserList />
-      <Nav />
-    </React.Fragment>
+    </>
   );
 }
