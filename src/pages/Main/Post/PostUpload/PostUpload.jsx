0695489cb78c6c3fcc36048d10de325576afa85c
konveloper <konveloper@gmail.com>
2022-12-19 15:29:36 +0900

    Feat : 텍스트 입력시 업로드 버튼 활성화 기능 추가

--- src/pages/Main/Post/PostUpload/PostUpload.jsx
+++ src/pages/Main/Post/PostUpload/PostUpload.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   PageWrapStyle,
   ConWrapStyle,
@@ -10,15 +10,37 @@ import profileImg from '../../../../assets/img/basic-profile-50.png';
 import UploadFileBtn from '../../../../components/Button/UploadFileBtn/UploadFileBtn';
 import Header from '../../../../components/Header/Header';
 
-export default function PostUpload() {
+function PostUpload() {
+  const [text, setText] = useState('');
+  const [isActive, setIsActive] = useState(false);
+
+  // 텍스트 입력
+  const OnChangeHandler = (e) => {
+    setText(e.target.value);
+  };
+
+  // 키 누르면 활성화
+  const ActivateHandler = () => {
+    if (text.length > 0) {
+      setIsActive(true);
+    } else {
+      setIsActive(false);
+    }
+  };
+
   return (
     <PageWrapStyle>
-      <Header size="ms" variant="disabled">
+      <Header size="ms" variant={!isActive ? 'disabled' : ''}>
         업로드
       </Header>
       <ConWrapStyle>
         <MyProfileImg src={profileImg} alt="" />
-        <PostTextStyle placeholder="게시글 입력하기" />
+        <PostTextStyle
+          placeholder="게시글 입력하기"
+          onChange={OnChangeHandler}
+          onKeyUp={ActivateHandler}
+          value={text}
+        />
         <BtnContainer>
           <UploadFileBtn />
         </BtnContainer>
@@ -26,3 +48,5 @@ export default function PostUpload() {
     </PageWrapStyle>
   );
 }
+
+export default PostUpload;
