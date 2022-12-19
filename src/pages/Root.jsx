e7c81b26bed063937eb0a0301e85dc82b06693f5
HYEBIN KIM <khv2644511@naver.com>
2022-12-19 23:03:47 +0900

    Modify : Nav 위치 변경

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -23,8 +23,8 @@ export default function Root() {
     <div>
       {auth ? (
         <PageWrapStyle>
-          <Nav />
           <Outlet />
+          <Nav />
         </PageWrapStyle>
       ) : (
         <Login />
