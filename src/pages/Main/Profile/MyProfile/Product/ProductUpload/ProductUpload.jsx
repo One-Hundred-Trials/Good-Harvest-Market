120b2e8a327bdcb0b6859dfceba3f9ffef4612a5
mixnuts211 <migyeong0211@gmail.com>
2022-12-21 21:06:22 +0900

    Feat : 상품 이름, 가격, url 데이터 post 작업

--- src/pages/Main/Profile/MyProfile/Product/ProductUpload/ProductUpload.jsx
+++ src/pages/Main/Profile/MyProfile/Product/ProductUpload/ProductUpload.jsx
@@ -1,24 +1,97 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useRecoilValue } from 'recoil';
 import Header from '../../../../../../components/Header/Header';
 import Input from '../../../../../../components/Input/Input';
 import ProductImgUpload from '../../../../../../components/ProductImgUpload/ProductImgUpload';
 import {
   ProductUploadContStyle,
-  ProductUploadSectionStyle,
+  ProductUploadMainStyle,
 } from './ProductUploadStyle';
+import API from '../../../../../../API';
+import { authAtom } from '../../../../../../_state/auth';
 
 export default function ProductUpload() {
+  const auth = useRecoilValue(authAtom);
+
+  const [itemName, setItemName] = useState('');
+  const [price, setPrice] = useState('');
+  const [link, setLink] = useState('');
+  const [itemImage, setitemImage] = useState('');
+
+  const itemNameHandler = (e) => {
+    setItemName(e.target.value);
+  };
+  const priceHandler = (e) => {
+    setPrice(e.target.value);
+  };
+  const linkHandler = (e) => {
+    setLink(e.target.value);
+  };
+
+  const productData = {
+    product: {
+      itemName,
+      price,
+      link,
+      itemImage,
+    },
+  };
+
+  const submitProductHandler = async () => {
+    try {
+      const res = await API.post('/product', JSON.stringify(productData), {
+        headers: {
+          Authorization: `Bearer ${auth}`,
+          'Content-type': 'application/json',
+        },
+        data: productData,
+      });
+      console.log(res);
+    } catch (err) {
+      if (err.response) {
+        // 응답코드 2xx가 아닌 경우
+        console.log(err.response.data);
+        console.log(err.response.status);
+        console.log(err.response.headers);
+      } else {
+        console.log(`Error: ${err.message}`);
+      }
+    }
+  };
+
   return (
     <ProductUploadContStyle>
-     <Header size="s" variant="disabled">
+      <Header size="ms" variant="disabled" onClick={submitProductHandler}>
         업로드
       </Header>
-      <ProductUploadSectionStyle>
+      <ProductUploadMainStyle>
         <ProductImgUpload />
-        <Input label="상품명" placeholder="2~15자 이내여야 합니다." />
-        <Input label="가격" placeholder="숫자만 입력 가능합니다." />
-        <Input label="판매 링크" placeholder="URL을 입력해 주세요." />
-      </ProductUploadSectionStyle>
+        <Input
+          label="상품명"
+          id="productName"
+          placeholder="2~15자 이내여야 합니다."
+          required="required"
+          min="2"
+          max="15"
+          onChange={itemNameHandler}
+        />
+        <Input
+          label="가격"
+          id="productPrice"
+          placeholder="숫자만 입력 가능합니다."
+          required="required"
+          min="2"
+          max="15"
+          onChange={priceHandler}
+        />
+        <Input
+          label="판매 링크"
+          id="productURL"
+          placeholder="URL을 입력해 주세요."
+          required="required"
+          onChange={linkHandler}
+        />
+      </ProductUploadMainStyle>
     </ProductUploadContStyle>
   );
 }
