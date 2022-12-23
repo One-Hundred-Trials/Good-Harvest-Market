4ca0c0291a205385d5f62b928fb0870fce28ebb1
khv2644511 <khv2644511@naver.com>
2022-12-23 17:36:06 +0900

    Modify : ProfileImgAccount 컴포넌트 주석처리

--- src/components/Chat/ChatListItem/ChatListItem.jsx
+++ src/components/Chat/ChatListItem/ChatListItem.jsx
@@ -8,14 +8,14 @@ import {
 export default function ChatListItem({ username, usertext, src }) {
   return (
     <ChatListContanierStyle>
-      <ProfileImgAccountStyle
+      {/* <ProfileImgAccountStyle
         width="42px"
         namemarginbottom="4px"
         margin="0 0 0 16px"
         src={src}
         username={username}
         usertext={usertext}
-      />
+      /> */}
       <DateSpan>2022.10.25</DateSpan>
     </ChatListContanierStyle>
   );
