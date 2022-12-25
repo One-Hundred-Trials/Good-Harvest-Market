eb9c72ad17948f70c8ae678cbe5a0e8b91095648
mixnuts211 <migyeong0211@gmail.com>
2022-12-25 22:16:18 +0900

    Style : Splash 이미지 마크업 및 스타일링 수정

--- src/pages/Splash/Splash.jsx
+++ src/pages/Splash/Splash.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import LogoImg from '../../assets/img/full-logo.png';
-import { SplashSectionStyle, WrapStyle } from './SplashStyle';
-import { motion } from 'framer-motion';
+import IntroLogoBg from '../../assets/img/intro-logo-bg.png';
+import IntroLogoLeaf from '../../assets/img/intro-logo-leaf.png';
+import IntroLogoText from '../../assets/img/intro-logo-text.png';
+import {
+  SplashArticleStyle,
+  WrapStyle,
+  IntroLogoBgStyle,
+  LeafContStyle,
+  IntroLogoLeafStyle,
+  IntroLogoTextStyle,
+} from './SplashStyle';
 
 export default function Splash() {
   return (
     <WrapStyle>
-      <SplashSectionStyle>
-        <img src={LogoImg} alt="풍년마켓" />
-      </SplashSectionStyle>
+      <SplashArticleStyle>
+        <IntroLogoBgStyle src={IntroLogoBg} alt="" />
+        <LeafContStyle>
+          <IntroLogoLeafStyle src={IntroLogoLeaf} alt="" />
+          <IntroLogoLeafStyle src={IntroLogoLeaf} alt="" />
+          <IntroLogoLeafStyle src={IntroLogoLeaf} alt="" />
+        </LeafContStyle>
+        <IntroLogoTextStyle src={IntroLogoText} alt="풍년마켓" />
+      </SplashArticleStyle>
     </WrapStyle>
   );
 }
