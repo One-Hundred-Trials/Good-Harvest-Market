fd19ebdd2b9980168707318002bb83bb36acbf13
mixnuts211 <migyeong0211@gmail.com>
2022-12-26 22:06:40 +0900

    Feat : 상품 alert창 노출 구현

--- src/components/Modal/ProductModalAlert/ProductModal.jsx
+++ src/components/Modal/ProductModalAlert/ProductModal.jsx
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ModalContainerDiv, ModalUl, ModalBtn } from './ProductModalStyle';
+import ProductAlert from './ProductAlert';
 
 export default function ModalSlide({ productId }) {
+  const [alert, setAlert] = useState(false);
+  const alertShow = () => {
+    setAlert(true);
+  };
+
   const navigate = useNavigate();
   const goEditHandler = () => {
     navigate(`/product/${productId}/edit`);
   };
   return (
-    <ModalContainerDiv>
-      <ModalUl>
-        <li>
-          <ModalBtn>삭제</ModalBtn>
-        </li>
-        <li>
-          <ModalBtn onClick={goEditHandler}>수정</ModalBtn>
-        </li>
-        <li>
-          <ModalBtn>웹사이트에서 상품 보기</ModalBtn>
-        </li>
-      </ModalUl>
-    </ModalContainerDiv>
+    <>
+      <ModalContainerDiv>
+        <ModalUl>
+          <li>
+            <ModalBtn onClick={alertShow}>삭제</ModalBtn>
+          </li>
+          <li>
+            <ModalBtn onClick={goEditHandler}>수정</ModalBtn>
+          </li>
+          <li>
+            <ModalBtn>웹사이트에서 상품 보기</ModalBtn>
+          </li>
+        </ModalUl>
+      </ModalContainerDiv>
+      {alert && <ProductAlert />}
+    </>
   );
 }
