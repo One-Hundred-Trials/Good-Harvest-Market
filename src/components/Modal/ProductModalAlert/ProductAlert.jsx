2aa4d670c18ac2fd55e4bae5c2a035624e5fe9b7
mixnuts211 <migyeong0211@gmail.com>
2022-12-27 14:22:07 +0900

    Feat : 상품 삭제 구현

--- src/components/Modal/ProductModalAlert/ProductAlert.jsx
+++ src/components/Modal/ProductModalAlert/ProductAlert.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import API from '../../../API';
+import { accountAtom, authAtom } from '../../../_state/auth';
 import {
   ModalAlertDiv,
   AlertBox,
@@ -8,9 +12,36 @@ import {
   AlertButtonRight,
 } from './ProductAlertStyle';
 
-export default function ModalAlert({ setAlert }) {
+export default function ModalAlert({ setAlert, productId }) {
+  const auth = useRecoilValue(authAtom);
+  const account = useRecoilValue(accountAtom);
   const alertClose = () => {
     setAlert(false);
+  };
+
+  const moveProfileHandler = () => {
+    window.location.replace(`/my_profile/${account}`);
+  };
+
+  const productDelHandler = async () => {
+    try {
+      const res = await API.delete(`/product/${productId}`, {
+        headers: {
+          'Content-type': 'application/json',
+          Authorization: `Bearer ${auth}`,
+        },
+      });
+      moveProfileHandler();
+    } catch (err) {
+      if (err.response) {
+        // 응답코드 2xx가 아닌 경우
+        console.log(err.response.data);
+        console.log(err.response.status);
+        console.log(err.response.headers);
+      } else {
+        console.log(`Error: ${err.message}`);
+      }
+    }
   };
 
   return (
@@ -19,7 +50,7 @@ export default function ModalAlert({ setAlert }) {
         <AlertHeader>상품을 삭제할까요?</AlertHeader>
         <AlertBody>
           <AlertButtonLeft onClick={alertClose}>취소</AlertButtonLeft>
-          <AlertButtonRight>삭제</AlertButtonRight>
+          <AlertButtonRight onClick={productDelHandler}>삭제</AlertButtonRight>
         </AlertBody>
       </AlertBox>
     </ModalAlertDiv>
