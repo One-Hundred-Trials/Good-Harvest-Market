9b0ea4ea17e65ea9eff3f319d0c1d17f2eea9fc3
khv2644511 <khv2644511@naver.com>
2022-12-28 10:45:11 +0900

    Modify : 주석 정리

--- src/pages/Main/Home/Home.jsx
+++ src/pages/Main/Home/Home.jsx
@@ -17,8 +17,6 @@ export default function Home() {
   const [post, setPost] = useState(null);
   const auth = useRecoilValue(authAtom);
   const [hasFollowing, setHasFollowing] = useState(null);
-
-  //  데이터 계속해서 담을 state
   const [pageNumber, setPageNumber] = useState(5);
   const [loading, setLoading] = useState(false);
   const target = useRef();
@@ -34,16 +32,13 @@ export default function Home() {
         },
       });
       const { posts } = res.data;
-      // setPins((prev) => [...prev, ...posts]);
       setPost(posts);
       setLoading(true);
-      //   console.log(posts);
       if (res.data.posts !== null) {
         setHasFollowing(true);
       }
     } catch (err) {
       if (err.response) {
-        // 응답코드 2xx가 아닌 경우
         console.log(err.response.data);
         console.log(err.response.status);
         console.log(err.response.headers);
@@ -64,7 +59,6 @@ export default function Home() {
         (entries) => {
           if (entries[0].isIntersecting) {
             loadMore();
-            // console.log(entries[0]);
           }
         },
         { threshold: 1 }
@@ -72,51 +66,6 @@ export default function Home() {
       observer.observe(target.current);
     }
   }, [loading]);
-
-  //   useEffect(() => {
-  //   let observer;
-  //   if (target) {
-  //     const onIntersect = async ([entry], observer) => {
-  //       if (entry.isIntersecting) {
-  //         observer.unobserve(entry.target);
-  //         await userFollowingData();
-  //         observer.observe(entry.target);
-  //       }
-  //     };
-  //     observer = new IntersectionObserver(onIntersect, { threshold: 1 });
-  //     observer.observe(target);
-  //   }
-  //   return () => observer && observer.disconnect();
-  // }, [target]);
-
-  // useEffect(() => {
-  //   const userFollowingData = async () => {
-  //     try {
-  //       const res = await API.get(`/post/feed?limit=5`, {
-  //         headers: {
-  //           'Content-Type': 'application/json',
-  //           Authorization: `Bearer ${auth}`,
-  //         },
-  //       });
-  //       const { posts } = res.data;
-  //       setPost(posts);
-  //       // console.log(posts);
-  //       if (res.data.posts !== null) {
-  //         setHasFollowing(true);
-  //       }
-  //     } catch (err) {
-  //       if (err.response) {
-  //         // 응답코드 2xx가 아닌 경우
-  //         console.log(err.response.data);
-  //         console.log(err.response.status);
-  //         console.log(err.response.headers);
-  //       } else {
-  //         console.log(`Error: ${err.message}`);
-  //       }
-  //     }
-  //   };
-  //   userFollowingData();
-  // }, [auth]);
 
   return (
     <>
