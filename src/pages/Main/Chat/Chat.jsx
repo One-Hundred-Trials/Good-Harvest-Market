395b171a50ca8ae713fb53b1de5ce184b3c34001
konveloper <konveloper@gmail.com>
2022-12-29 05:11:18 +0900

    Feat : 채팅리스트에서 채팅룸으로 이동하는 기능 추가

--- src/pages/Main/Chat/Chat.jsx
+++ src/pages/Main/Chat/Chat.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import ChatListItem from '../../../components/ChatListItem/ChatListItem';
 import Header from '../../../components/Header/Header';
 import { ConWrap } from '../../../styles/GlobalStyles';
@@ -17,22 +18,27 @@ export default function Chat() {
     <>
       <Header />
       <ChatWrapStyle>
-        <ChatListItem
-          user="옆동네 퐁이"
-          content="채소가 참 싱싱하네요~ 직거래도 가능하신가요?"
-          date="2023.01.06"
-        />
-
-        <ChatListItem
-          user="애월읍 감귤 농장"
-          content="올해 귤이 참 맛있게 나왔어요 ㅎㅎ"
-          date="2022.10.21"
-        />
-        <ChatListItem
-          user="주말엔 내가 감자 농부"
-          content="다음에 감자 나눔해드릴게요 좋은 하루 되세요 ^^"
-          date="2022.08.29"
-        />
+        <Link to="/chat/1">
+          <ChatListItem
+            user="옆동네 퐁이"
+            content="채소가 참 싱싱하네요~ 직거래도 가능하신가요?"
+            date="2023.01.06"
+          />
+        </Link>
+        <Link to="">
+          <ChatListItem
+            user="애월읍 감귤 농장"
+            content="올해 귤이 참 맛있게 나왔어요 ㅎㅎ"
+            date="2022.10.21"
+          />
+        </Link>
+        <Link to="">
+          <ChatListItem
+            user="주말엔 내가 감자 농부"
+            content="다음에 감자 나눔해드릴게요 좋은 하루 되세요 ^^"
+            date="2022.08.29"
+          />
+        </Link>
       </ChatWrapStyle>
     </>
   );
