92dc4108c5551e5f321577387dc0c001cd0d8f88
mixnuts211 <migyeong0211@gmail.com>
2022-12-28 23:31:54 +0900

    Add : 댓글 시간 데이터 추가

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -18,6 +18,27 @@ export default function Comment({ comment, postId, deleteComment }) {
   const auth = useRecoilValue(authAtom);
   const [modal, setModal] = useState(false);
   const [commentText, setReportComment] = useState(`${comment.content}`);
+
+  const detailDate = (a) => {
+    const milliSeconds = new Date() - a;
+    const seconds = milliSeconds / 1000;
+    if (seconds < 60) return `방금 전`;
+    const minutes = seconds / 60;
+    if (minutes < 60) return `${Math.floor(minutes)}분 전`;
+    const hours = minutes / 60;
+    if (hours < 24) return `${Math.floor(hours)}시간 전`;
+    const days = hours / 24;
+    if (days < 7) return `${Math.floor(days)}일 전`;
+    const weeks = days / 7;
+    if (weeks < 5) return `${Math.floor(weeks)}주 전`;
+    const months = days / 30;
+    if (months < 12) return `${Math.floor(months)}개월 전`;
+    const years = days / 365;
+    return `${Math.floor(years)}년 전`;
+  };
+
+  const nowDate = detailDate(new Date(comment.createdAt));
+
   const modalUp = () => {
     setModal(true);
   };
@@ -86,7 +107,7 @@ export default function Comment({ comment, postId, deleteComment }) {
           <InfoDiv>
             <ProfileImg src={comment.author.image} alt="" />
             <strong>{comment.author.username}</strong>
-            <span>{comment.createdAt}</span>
+            <span>{nowDate}</span>
           </InfoDiv>
           <MoreBtn onClick={modalUp}>
             <img src={iconMoreImg} alt="" />
