f0789b9c44432306dc4f59eea40a0c6e98280879
konveloper <konveloper@gmail.com>
2023-01-01 23:28:22 +0900

    Modify : 각주 삭제 및 navigate 코드 수정

--- src/pages/Main/Post/PostUpload/PostUpload.jsx
+++ src/pages/Main/Post/PostUpload/PostUpload.jsx
@@ -22,17 +22,12 @@ function PostUpload() {
   const auth = useRecoilValue(authAtom);
   const account = useRecoilValue(accountAtom);
   const navigate = useNavigate();
-  // 프로필 이미지
   const [profileImg, setProfileImg] = useState('');
-  // 버튼 활성화
   const [isActive, setIsActive] = useState(false);
-  // post 정보 데이터
   const [text, setText] = useState('');
   const [imgFile, setImgFile] = useState('');
-  // 이미지 미리보기 데이터
   const [previewImgUrl, setPreviewImgUrl] = useState('');
 
-  // 내 프로필 이미지 불러오기
   useEffect(() => {
     const getMyProfileImg = async () => {
       try {
@@ -44,7 +39,6 @@ function PostUpload() {
         setProfileImg(res.data.user.image);
       } catch (err) {
         if (err.response) {
-          // 응답코드 2xx가 아닌 경우
           console.log(err.response.data);
           console.log(err.response.status);
           console.log(err.response.headers);
@@ -56,7 +50,6 @@ function PostUpload() {
     getMyProfileImg();
   }, []);
 
-  // 텍스트를 입력하거나 이미지가 있으면 버튼 활성화
   useEffect(() => {
     if (text || previewImgUrl) {
       setIsActive(true);
@@ -65,14 +58,11 @@ function PostUpload() {
     }
   }, [text, previewImgUrl]);
 
-  // input에 입력한 값 알아내서 저장
   const textChangeHandler = (e) => {
     setText(e.target.value);
   };
 
-  // 이미지 업로드
   const imgUploadHandler = async (file) => {
-    // const file = e.target.files[0];
     const formData = new FormData();
     formData.append('image', file);
     try {
@@ -85,24 +75,20 @@ function PostUpload() {
       return feedImgUrl;
     } catch (err) {
       if (err.response) {
-        // 응답코드 2xx가 아닌 경우
         console.log(err.response.data);
         console.log(err.response.status);
         console.log(err.response.headers);
       } else {
         console.log(`Error: ${err.message}`);
       }
-      return false;
+      return null;
     }
   };
 
-  // 이미지 미리보기
   const PreviewImgHandler = (e) => {
-    e.preventDefault();
-    const file = e.target.files[0];
     const correctForm = /(.*?)\.(jpg|gif|png|jpeg|bmp|tif|heic|)$/;
+    const file = e.target.files[0];
     const fileReader = new FileReader();
-    console.log(file.type);
     if (file.length > 0) {
       alert('1개의 이미지 파일을 업로드 하세요.');
       return;
@@ -125,14 +111,12 @@ function PostUpload() {
     };
   };
 
-  // 이미지 미리보기 삭제
   const DeletePreviewImgHandler = (e) => {
     e.preventDefault();
     setPreviewImgUrl('');
     setImgFile('');
   };
 
-  // 게시글 업로드
   const postUploadHandler = async () => {
     const image = await imgUploadHandler(imgFile);
     const postData = {
@@ -152,10 +136,9 @@ function PostUpload() {
           'Content-type': 'application/json',
         },
       });
-      res.then(navigate(`/my_profile/${account}`));
+      navigate(`/my_profile/${account}`);
     } catch (err) {
       if (err.response) {
-        // 응답코드 2xx가 아닌 경우
         console.log(err.response.data);
         console.log(err.response.status);
         console.log(err.response.headers);
