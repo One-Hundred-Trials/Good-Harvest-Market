e412f403733aee3b863066d3dc846881c59f248a
mixnuts211 <migyeong0211@gmail.com>
2023-01-02 22:12:08 +0900

    Modify : font 서브셋으로 변경

--- src/styles/GlobalStyles.jsx
+++ src/styles/GlobalStyles.jsx
@@ -4,7 +4,41 @@ import reset from 'styled-reset';
 export const GlobalStyle = createGlobalStyle`
     ${reset};
 
-    @import url('//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css');
+    @font-face {
+      font-family: 'Spoqa Han Sans';
+      font-weight: 700;
+      src: local('Spoqa Han Sans Bold'),
+      url('../fonts/SpoqaHanSans/SpoqaHanSansBold.woff2') format('woff2'),
+      url('../fonts/SpoqaHanSans/SpoqaHanSansBold.woff') format('woff'),
+      url('../fonts/SpoqaHanSans/SpoqaHanSansBold.ttf') format('truetype');
+    }
+
+    @font-face {
+      font-family: 'Spoqa Han Sans';
+      font-weight: 400;
+      src: local('Spoqa Han Sans Regular'),
+      url('../fonts/SpoqaHanSans/SpoqaHanSansRegular.woff2') format('woff2'),
+      url('../fonts/spoqa-han-sans/SpoqaHanSans/SpoqaHanSansRegular.woff') format('woff'),
+      url('../fonts/spoqa-han-sans/SpoqaHanSans/SpoqaHanSansRegular.ttf') format('truetype');
+    }
+
+    @font-face {
+      font-family: 'Spoqa Han Sans';
+      font-weight: 300;
+      src: local('Spoqa Han Sans Light'),
+      url('../fonts/SpoqaHanSans/SpoqaHanSansLight.woff2') format('woff2'),
+      url('../fonts/SpoqaHanSans/SpoqaHanSansLight.woff') format('woff'),
+      url('../fonts/SpoqaHanSans/SpoqaHanSansLight.ttf') format('truetype');
+    }
+
+    @font-face {
+      font-family: 'Spoqa Han Sans';
+      font-weight: 100;
+      src: local('Spoqa Han Sans Thin'),
+      url('../fonts/SpoqaHanSans/SpoqaHanSansThin.woff2') format('woff2'),
+      url('../fonts/SpoqaHanSans/SpoqaHanSansThin.woff') format('woff'),
+      url('../fonts/SpoqaHanSans/SpoqaHanSansThin.ttf') format('truetype');
+    }
 
     * {
       box-sizing: border-box;
@@ -23,7 +57,7 @@ export const GlobalStyle = createGlobalStyle`
       --bg-color: #E5E5E5;
     }
     body {
-      font-family: "Spoqa Han Sans Neo", "sans-serif";
+      font-family: "Spoqa Han Sans", "sans-serif";
       -ms-overflow-style: none;
       scrollbar-width: none;
       &::-webkit-scrollbar {
