d9c5c5ffff59f58ad846ed1609b5d82a0e895583
mixnuts211 <migyeong0211@gmail.com>
2023-01-02 11:48:12 +0900

    Add : 404 페이지, 홈 피드 유저 검색하러가기 버튼 이동 경로 추가

--- src/components/HomeRender/HomeRenderBlank.jsx
+++ src/components/HomeRender/HomeRenderBlank.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 import Blank from '../Blank/Blank';
 import DuckBlank from '../../assets/img/duck-blank.png';
 
@@ -13,12 +14,15 @@ const BlankWrap = styled.section`
 `;
 
 export default function HomeRenderBlank() {
+  const navigate = useNavigate();
+
   return (
     <BlankWrap>
       <Blank
         src={DuckBlank}
         width="180px"
         text="유저를 검색해 팔로우 해보세요!"
+        clickHandler={() => navigate('/search')}
       >
         검색하기
       </Blank>
