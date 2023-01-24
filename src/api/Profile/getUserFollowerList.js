42e09be375bfb19494f5c183a6ef864343a986a8
khv2644511 <khv2644511@naver.com>
2023-01-25 02:41:39 +0900

    Modify : 파일명 변경

---
+++ src/api/Profile/getUserFollowerList.js
@@ -0,0 +1,20 @@
+import { axiosPrivate } from '../api';
+
+const getUserFollowerList = async (account) => {
+  try {
+    const response = await axiosPrivate.get(
+      `/profile/${account}/follower?limit=100`
+    );
+    return response.data;
+  } catch (err) {
+    if (err.response) {
+      console.log(err.response.data);
+      console.log(err.response.status);
+      console.log(err.response.headers);
+    } else {
+      console.log(`Error: ${err.message}`);
+    }
+  }
+  return null;
+};
+export default getUserFollowerList;
