9650751f9a47fba1fcd269d70b87318c54117cb5
konveloper <konveloper@gmail.com>
2023-01-26 23:24:49 +0900

    Modify : 좋아요 버튼 api 파일 err 처리

--- src/components/HeartIcon/HeartIcon.jsx
+++ src/components/HeartIcon/HeartIcon.jsx
@@ -6,43 +6,22 @@ import HeartOn from '../../assets/img/icon-heart-on.png';
 import { HeartStyle, HeartImgStyle } from './HeartIconStyle';
 
 export default function HeartIcon({
-  heartCount,
-  like,
   postId,
+  like,
+  heartCount,
   setLike,
   setLikeCount,
 }) {
   const heartOnHandler = async () => {
-    try {
-      const res = await postLiked(postId);
-      setLike(res.post.hearted);
-      setLikeCount(res.post.heartCount);
-    } catch (err) {
-      if (err.response) {
-        console.log(err.response.data);
-        console.log(err.response.status);
-        console.log(err.response.headers);
-      } else {
-        console.log(`Error: ${err.message}`);
-      }
-    }
+    const res = await postLiked(postId);
+    setLike(res.post.hearted);
+    setLikeCount(res.post.heartCount);
   };
 
   const heartOffHandler = async () => {
-    try {
-      const res = await deleteLiked(postId);
-      console.log(res);
-      setLike(res.post.hearted);
-      setLikeCount(res.post.heartCount);
-    } catch (err) {
-      if (err.response) {
-        console.log(err.response.data);
-        console.log(err.response.status);
-        console.log(err.response.headers);
-      } else {
-        console.log(`Error: ${err.message}`);
-      }
-    }
+    const res = await deleteLiked(postId);
+    setLike(res.post.hearted);
+    setLikeCount(res.post.heartCount);
   };
 
   const handleClick = () => {
