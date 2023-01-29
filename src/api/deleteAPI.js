aba84d3ecba561994dbb961c19185b619a6e4954
konveloper <konveloper@gmail.com>
2023-01-29 23:05:36 +0900

    modify : 공통 getAPI, putAPI, deleteAPI 삭제

--- src/api/deleteAPI.js
+++
@@ -1,7 +0,0 @@
-import { baseUrl, axiosPrivate } from './api';
-
-const deleteAPI = async (url) => {
-  const response = await axiosPrivate.delete(baseUrl + url);
-  return response.data;
-};
-export default deleteAPI;
--- src/api/getAPI.js
+++
@@ -1,9 +0,0 @@
-import { baseUrl, axiosPrivate } from './api';
-
-const getAPI = async (url) => {
-  const response = await axiosPrivate.get(baseUrl + url);
-  console.log(response);
-  return response.data;
-};
-
-export default getAPI;
