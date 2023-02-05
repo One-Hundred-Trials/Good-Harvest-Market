34902349820ed0b1bc046c8e7f6e92712a5051f6
khv2644511 <khv2644511@naver.com>
2023-02-05 23:11:11 +0900

    Refactor: ShareIcon  스타일파일 분리

--- src/components/ShareIcon/ShareIcon.jsx
+++ src/components/ShareIcon/ShareIcon.jsx
@@ -1,29 +1,6 @@
 import React, { useRef } from 'react';
-import styled from 'styled-components';
 import iconShare from 'assets/img/icon-share.png';
-
-const ShareBtn = styled.button`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 34px;
-  height: 34px;
-  border: 1px solid #dbdbdb;
-  border-radius: 50%;
-  img {
-    width: 15px;
-    height: 15px;
-  }
-`;
-
-const TextArea = styled.textarea`
-  position: absolute;
-  width: 0px;
-  height: 0px;
-  bottom: 0;
-  left: 0;
-  opacity: 0;
-`;
+import { ShareBtn, TextArea } from './ShareIconStyle';
 
 export default function ShareIcon() {
   const copyUrlRef = useRef();
