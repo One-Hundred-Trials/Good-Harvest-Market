368215812adc7b8090b94665343e506a065d50b0
khv2644511 <khv2644511@naver.com>
2023-02-05 16:54:21 +0900

    Modify: NotFound  절대경로 수정

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
-import icon404Img from '../../assets/img/icon-404.png';
-import Blank from '../../components/Blank/Blank';
+import icon404Img from 'assets/img/icon-404.png';
+import Blank from 'components/Blank/Blank';
 
 const ContainerSectionStyle = styled.section`
   position: absolute;
