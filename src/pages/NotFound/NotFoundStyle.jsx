10ca04a27979c82d99010b58f543e714916be1d7
khv2644511 <khv2644511@naver.com>
2023-02-05 23:51:44 +0900

    Refactor: NotFound  스타일파일 분리

---
+++ src/pages/NotFound/NotFoundStyle.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const ContainerSectionStyle = styled.section`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 0 72px;
+  text-align: center;
+`;
+
+export default ContainerSectionStyle;
