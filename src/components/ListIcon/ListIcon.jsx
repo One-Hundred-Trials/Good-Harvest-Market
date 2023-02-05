eaa4209a871e1b8d4282538f395fab8de28d9b0c
khv2644511 <khv2644511@naver.com>
2023-02-05 19:15:14 +0900

    Refactor: ListIcon  스타일파일 분리

--- src/components/ListIcon/ListIcon.jsx
+++ src/components/ListIcon/ListIcon.jsx
@@ -1,12 +1,7 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
 import IconPostListOn from '../../assets/img/icon-post-list-on.png';
 import iconPostListOff from '../../assets/img/icon-post-list-off.png';
-
-const BtnStyle = styled.button`
-  width: 26px;
-  height: 26px;
-`;
+import BtnStyle from './ListIconStyle';
 
 export default function ListIcon({ toggle, onclick }) {
   return (
