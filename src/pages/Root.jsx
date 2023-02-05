70113f00f03d29c7d4fdd754f726be1b79263933
khv2644511 <khv2644511@naver.com>
2023-02-05 23:55:14 +0900

    Refactor: Root  스타일파일 분리

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
 import { useRecoilValue } from 'recoil';
 import { Outlet, Navigate } from 'react-router-dom';
 import { authAtom } from '_state/auth';
 import Nav from 'components/common/Nav/Nav';
-import { PageWrap } from 'styles/GlobalStyles';
-
-const PageWrapStyle = styled.div`
-  ${PageWrap}
-`;
+import PageWrapStyle from './RootStyle';
 
 export default function Root() {
   const auth = useRecoilValue(authAtom);
---
+++ src/pages/RootStyle.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { PageWrap } from '../styles/GlobalStyles';
+
+const PageWrapStyle = styled.div`
+  ${PageWrap}
+`;
+
+export default PageWrapStyle;
