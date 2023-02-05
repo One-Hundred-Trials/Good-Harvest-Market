8850bce5dcc6f5a3cb2dc6f2a1f1bcbdd7cc0755
khv2644511 <khv2644511@naver.com>
2023-02-05 23:51:44 +0900

    Refactor: NotFound  스타일파일 분리

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,17 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import icon404Img from 'assets/img/icon-404.png';
 import Blank from 'components/Blank/Blank';
-
-const ContainerSectionStyle = styled.section`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  padding: 0 72px;
-  text-align: center;
-`;
+import ContainerSectionStyle from './NotFoundStyle';
 
 export default function NotFound() {
   const navigate = useNavigate();
