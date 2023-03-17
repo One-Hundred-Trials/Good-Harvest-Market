d1911543b7340e5ed812c47e7db416554c075049
khv2644511 <khv2644511@naver.com>
2023-03-18 00:03:23 +0900

    Feat: Firebase 초기 셋팅

---
+++ src/api/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
